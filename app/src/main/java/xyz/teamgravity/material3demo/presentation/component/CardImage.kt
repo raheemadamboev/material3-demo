4e2406ef66e9813c1fad9e39f45a75e10114758d
raheemadamboev <vividricemaryandmax@gmail.com>
2022-06-05 19:17:57 +0500

    build card image

---
+++ app/src/main/java/xyz/teamgravity/material3demo/presentation/component/CardImage.kt
@@ -0,0 +1,92 @@
+package xyz.teamgravity.material3demo.presentation.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.FavoriteBorder
+import androidx.compose.material.icons.outlined.Share
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberAsyncImagePainter
+import com.google.accompanist.flowlayout.FlowRow
+import com.google.accompanist.flowlayout.SizeMode
+import xyz.teamgravity.material3demo.R
+import kotlin.random.Random
+
+@Composable
+fun CardImage(
+    title: String,
+    description: String,
+    modifier: Modifier = Modifier,
+) {
+    Card(
+        shape = MaterialTheme.shapes.large,
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surfaceVariant
+        ),
+        modifier = Modifier
+    ) {
+        Image(
+            painter = rememberAsyncImagePainter(model = "https://picsum.photos/seed/${Random.nextInt()}/300/200"),
+            contentDescription = stringResource(id = R.string.cd_image),
+            modifier = Modifier
+                .clip(MaterialTheme.shapes.large)
+                .fillMaxWidth()
+                .aspectRatio(3F / 2F)
+        )
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Text(
+                text = title,
+                style = MaterialTheme.typography.titleLarge
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = description,
+                style = MaterialTheme.typography.bodyMedium
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            FlowRow(
+                mainAxisSize = SizeMode.Wrap,
+                mainAxisSpacing = 8.dp,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                AssistChip(
+                    onClick = { },
+                    colors = AssistChipDefaults.assistChipColors(
+                        leadingIconContentColor = MaterialTheme.colorScheme.onSurfaceVariant
+                    ),
+                    leadingIcon = {
+                        Icon(
+                            imageVector = Icons.Outlined.FavoriteBorder,
+                            contentDescription = stringResource(id = R.string.cd_favorite)
+                        )
+                    },
+                    label = {
+                        Text(text = stringResource(id = R.string.mark_as_favorite))
+                    }
+                )
+                AssistChip(
+                    onClick = { },
+                    colors = AssistChipDefaults.assistChipColors(
+                        leadingIconContentColor = MaterialTheme.colorScheme.onSurfaceVariant
+                    ),
+                    leadingIcon = {
+                        Icon(
+                            imageVector = Icons.Outlined.Share,
+                            contentDescription = stringResource(id = R.string.cd_share)
+                        )
+                    },
+                    label = {
+                        Text(text = stringResource(id = R.string.share_with_others))
+                    }
+                )
+            }
+        }
+    }
+}
